782855f83376c232593dd599c995d41c4e9b8084
dmitriy-petrov-swi <dmitriy.petrov@solarwinds.com>
2023-07-10 17:08:43 +0300

    UC#14 Infrastructure setup

---
+++ i18n_l10n_MVC_NETCore/i18n_l10n_MVC_NETCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace i18n_l10n_MVC_NETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
